4a0747977958029693228f436be1cbb8d53fb8df
macsofia <sofia.blanco12@educa.madrid.org>
2025-02-06 14:00:15 +0100

    coment2

--- repositorioGit/HolaMundoGit.java
+++
@@ -1,7 +0,0 @@
-public class HolaMundoGit {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
-        System.out.println("001");
-        System.out.println("change one");
-    }
-}
